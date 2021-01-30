58e898c314c1449d9c6130ba1df42ac8e9b5a793
Andrey Markov <alyldas@ya.ru>
2021-01-30 23:34:10 +0300

    Update oxford-test.js

--- scripts/oxford-test.js
+++ scripts/oxford-test.js
@@ -339,9 +339,6 @@ const userInfoForm = {
 function calculateResults() {
     if (ranges) {
         document.removeEventListener("keydown", keyDownSwitch, false);
-        
-        //console.log(ranges);
-        //console.log(capacityResults);
 
         capacityResults.A = ranges.A[capacityAnswers.A];
         capacityResults.B = ranges.B[capacityAnswers.B];
@@ -353,8 +350,6 @@ function calculateResults() {
         capacityResults.H = ranges.H[capacityAnswers.H];
         capacityResults.I = ranges.I[capacityAnswers.I];
         capacityResults.J = ranges.J[capacityAnswers.J];
-        
-        //console.log(capacityResults);
 
         if (capacityResults.A === undefined) capacityResults.A = 100;
         if (capacityResults.B === undefined) capacityResults.B = 100;
@@ -366,8 +361,6 @@ function calculateResults() {
         if (capacityResults.H === undefined) capacityResults.H = 100;
         if (capacityResults.I === undefined) capacityResults.I = 100;
         if (capacityResults.J === undefined) capacityResults.J = 100;
-        
-        //console.log(capacityResults);
     } else {
         alert("Ranges еще не загружены"); // Диагностическое сообщение
     }
@@ -862,7 +855,7 @@ function submitUserInfo(event) {
 }
 
 export function startTest() {
-    console.log("v1.0.3");
+    console.info("Script v1.0.4");
     
     loadUserInfoFromLocalStorage();
 
